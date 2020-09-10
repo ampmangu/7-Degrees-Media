7a84d6c8995d88275e5be02111640773a8fbcbdd
Adrian Marin <adrianmarinportillo@gmail.com>
2020-09-10 18:51:22 +0200

    First commit

---
+++ src/main/kotlin/com/ampmangu/degreesmedia/DegreesMediaApplication.kt
@@ -0,0 +1,11 @@
+package com.ampmangu.degreesmedia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DegreesMediaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DegreesMediaApplication>(*args)
+}
---
+++ src/test/kotlin/com/ampmangu/degreesmedia/DegreesMediaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ampmangu.degreesmedia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DegreesMediaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
