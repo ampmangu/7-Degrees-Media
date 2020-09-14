cbc447c8d8c9f68c216a1cac802638e08310fc0a
Adrian Marin <adrianmarinportillo@gmail.com>
2020-09-14 21:18:33 +0200

    7DM-1 Basic config done

--- src/main/kotlin/com/ampmangu/degreesmedia/DegreesMediaApplication.kt
+++ src/main/kotlin/com/ampmangu/degreesmedia/DegreesMediaApplication.kt
@@ -1,11 +1,25 @@
 package com.ampmangu.degreesmedia
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.core.env.ConfigurableEnvironment
 
 @SpringBootApplication
+@EnableConfigurationProperties(LiquibaseProperties::class)
 class DegreesMediaApplication
 
+val log: Logger = LoggerFactory.getLogger(DegreesMediaApplication::class.java)
 fun main(args: Array<String>) {
-	runApplication<DegreesMediaApplication>(*args)
+	val app = SpringApplication(DegreesMediaApplication::class.java)
+	val env = app.run(*args).environment
+	logApplicationStartup(env)
+}
+
+fun logApplicationStartup(env: ConfigurableEnvironment) {
+	log.info("Hello there")
+
 }
